b8bb51782b1a2814bc7eb3be7cf758e345892523
Kris Turner <13710532+kristopherjturner@users.noreply.github.com>
2026-08-24 16:03:03 -0400

    feat(themes): update static bundle with 6 official built-in themes (AB#6167 AB#6292)

---
+++ _next/static/chunks/layout-segment-context-DutidEkz.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{r as t}from"./framework-BQUlGUOV.js";import{s as n}from"./navigation-errors-CoFGy5r3.js";import"./index-BXf9pgss.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
